253d8cb6bd53ceb09519458d059e6d464d573637
gpasha <g1ebikpasha@gmail.com>
2021-07-21 21:06:41 +0300

    active links, title

--- index.js
+++ index.js
@@ -14,13 +14,25 @@ app.set('views', 'views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Home page',
+        isHome: true
+    })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: 'Create a course',
+        isAdd: true
+    })
 })
 
+app.get('/courses', (req, res) => {
+    res.render('courses', {
+        title: "Courses",
+        isCourses: true
+    })
+})
 
 
 
