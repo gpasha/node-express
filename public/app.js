a50a787749fd750a8afc3a2671ce6dc48aad8122
gpasha <g1ebikpasha@gmail.com>
2021-07-22 20:01:54 +0300

    rerender basket page after remove a course

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('be-BE', {
+const toCurrency = price => {
+    return new Intl.NumberFormat('be-BE', {
         currency: 'byn',
         style: 'currency'
-    }).format(node.textContent)
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = toCurrency(node.textContent)
 })
 
 const $card = document.querySelector('#card')
@@ -16,7 +20,23 @@ if ($card) {
                 method: 'delete'
             }).then(res => res.json())
               .then(data => {
-                    console.log(data);
+                    if (data.courses.length) {
+                        const html = data.courses.map(course => {
+                            return `
+                                <tr>
+                                    <td>${course.title}</td>
+                                    <td>${course.count}</td>
+                                    <td>
+                                        <buttom class="btn btn-primary js-remove" data-id=${course.id}>Remove</buttom>
+                                    </td>
+                                </tr>
+                            `
+                        }).join('')
+                        document.querySelector('tbody').innerHTML = html
+                        document.querySelector('.price').textContent = toCurrency(data.price)
+                    } else {
+                        $card.innerHTML = '<p>The basket is empty</p>'
+                    }
                })
         }
     })
