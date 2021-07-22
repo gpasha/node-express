026758788e723b816fec1f43d15f64583cc45697
gpasha <g1ebikpasha@gmail.com>
2021-07-22 15:31:54 +0300

    course page, new layout, set up it all

--- routers/courses.js
+++ routers/courses.js
@@ -3,13 +3,20 @@ const router = Router()
 const Course = require('../models/course')
 
 router.get('/', async (req, res) => {
-
     const courses = await Course.getAll()
-
     res.render('courses', {
         title: 'Courses',
         isCourses: true,
         courses
+    })
+})
+
+router.get('/:id', async (req, res) => {
+    const course = await Course.getById(req.params.id)
+    res.render('course', {
+        layout: 'empty',
+        title: `Course: ${course.title}`,
+        course
     })
 })
 
