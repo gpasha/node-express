0d13cbec5a12a6f9d29d36f163e92e57974e0fd6
gpasha <g1ebikpasha@gmail.com>
2021-07-23 15:57:56 +0300

    mongoose, mongoDB, course model, handlebars,

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const path = require('path')
-const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const home = require('./routers/home')
 const add = require('./routers/add')
 const courses = require('./routers/courses')
 const card = require('./routers/card')
-const app = express()
 
-const hbs = exphbs.create({
+/* START new setting express-handlebars */
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+const app = express();
+
+const hbs = expressHandlebars.create({
     defaultLayout: 'main',
-    extname: 'hbs'
+    extname: 'hbs',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
 })
 
 app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 app.set('views', 'views')
+/* END new setting express-handlebars */
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true})) // to get data for POST method
@@ -30,6 +38,20 @@ app.use('/card', card)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+    try {
+        // const url = 'mongodb+srv://g_pavel:36bCGx5i4gcEQA2@cluster0.dpa5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        const url = 'mongodb+srv://g_pavel:36bCGx5i4gcEQA2@cluster0.dpa5t.mongodb.net/shop'
+        await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch(e) {
+        console.log('error: ', e);
+    }
+}
+
+start()
+
+
+
